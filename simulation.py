271fee4a48a52a1bf7352b8e6a6f18b11acb8343
Fabian Krautgasser <fkrautga@iup.uni-heidelberg.de>
2018-01-09 18:49:10 +0100

    small change in terminal output

--- simulation.py
+++ simulation.py
@@ -3,8 +3,8 @@ import yaml
 import ABM as abm
 import visualisations as vis
 import matplotlib.pyplot as plt
-import numpy as np 
-import datetime as dt 
+import numpy as np
+import datetime as dt
 
 with open("simconfig.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
@@ -12,7 +12,7 @@ with open("simconfig.yml", 'r') as ymlfile:
 #print(cfg['Sim']['Pmate']['Pred'])
 
 Agents = dict() # initialize empty agent dictionary
-newborn = dict() # initialize empty newborn dictionary 
+newborn = dict() # initialize empty newborn dictionary
 
 # fixed seed for reproducability
 np.random.seed(12345678)
@@ -21,7 +21,7 @@ np.random.seed(12345678)
 w = cfg['Grid']['NX']
 h = cfg['Grid']['NY']
 
-# general 
+# general
 food_res = 4
 
 # initialize grid
@@ -30,7 +30,7 @@ grid = abm.Grid(w,h)
 # initialize number of prey and predators
 rhoPrey = cfg['Sim']['RhoPrey']
 rhoPred = cfg['Sim']['RhoPred']
-CarryingCapacity = grid.get_cc() 
+CarryingCapacity = grid.get_cc()
 nprey = int(CarryingCapacity * rhoPrey)
 npred = int(CarryingCapacity * rhoPred)
 num_agents = npred + nprey
@@ -46,26 +46,29 @@ for _ in range(npred):
 for _ in range(npred, num_agents):
     abm.Prey(Agents, food_res, ipos[_], MaxFoodReserve=8)
 
-################# actual simulation below here ############################## 
+################# actual simulation below here ##############################
 
 if(__name__ == '__main__'):
     print(":: Simulation start", dt.datetime.now())
+    fig, ax = vis.show_agents(grid, Agents, savefig=True, title="Init")
+    plt.close(fig)
     for _ in range(cfg['Sim']['NEpoch']):
         now = str(dt.datetime.now()).split(" ")[1].split(".")[0]
-        print(": Simulation step: ", _, " -- ", now)
+        print("::: Step: ", _, " -- ", now)
+        print(":::: Mean food reserve: ", vis.mean_food(Agents))
         agentkeys = list(Agents.keys())
-        np.random.shuffle(agentkeys) 
+        np.random.shuffle(agentkeys)
         for ID in agentkeys:
             a = Agents[ID]
             if(a.get_kin() is not None):
-                Nbh, NbhAgents, currentPos = grid.get_NbhAgents(a, Agents)    
+                Nbh, NbhAgents, currentPos = grid.get_NbhAgents(a, Agents)
                 roll = np.random.rand()
                 if(roll < a.get_pBreed()):
                     a.createOffspring(grid, Agents, newborn)
                 else:
                     a.Eat(grid, Nbh, NbhAgents, currentPos, Agents)
         abm.lifecycle(grid, Agents, newborn)  # cleanup all the dead Agents
-    
+
         fig, ax = vis.show_agents(grid, Agents, savefig=True, title="timestep " + str(_))
         plt.close(fig)
     print(":: Simulation stop", dt.datetime.now())
