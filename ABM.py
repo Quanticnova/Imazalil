609a9b5be5f5acb1183644be4f01e64789f2f7fb
Fabian Krautgasser <fkrautgasser@posteo.org>
2018-01-16 00:41:51 +0100

    lots of comments and some optimization with numpy magic

--- ABM.py
+++ ABM.py
@@ -8,7 +8,13 @@ import datetime as dt
 
 class Agent:
     """
-    This class provides stuff for more stuff to be stuffed.
+    This class provides an object, which can have the following attributes:
+        - FoodReserve
+        - MaxFoodReserve
+        - Generation
+        - ID
+    It is also equipped with getter functions for each of the attributes, and a setter function for
+    the FoodReserve.
     """
 
     # slots are used to fix the number of attributes a class can have -> memory/speed improvement
@@ -20,7 +26,6 @@ class Agent:
         self._MaxFoodReserve = MaxFoodReserve
         self._Generation = Generation
         self._ID = str(uuid.uuid4())
-        #self._Kin = None
 
     def get_ID(self):
         """
@@ -58,53 +63,107 @@ class Prey(Agent):
     This class is derived from the Agent class.
     """
 
-    __slots__ = ['_FoodReserve', '_MaxFoodReserve', '_Generation', '_ID']
+    __slots__ = ['_FoodReserve', '_MaxFoodReserve', '_Generation', '_ID', '_pBreed', '_pFlee']
 
-    def __init__(self, FoodReserve, Generation=0, MaxFoodReserve=None, pBreed=0.2):
+    def __init__(self, FoodReserve, Generation=0, MaxFoodReserve=None, pBreed=0.2, pFlee=0.5):
         super().__init__(FoodReserve, Generation, MaxFoodReserve)
         self._ID = "B" + self._ID  # Bfor beute. this kinda makes the whole kin thing obsolete.
+        self._pBreed = pBreed
+        self._pFlee = pFlee
+
+    def get_pFlee(self):
+        """
+        getter function for probability to flee.
+        """
+        return self._pFlee
+
+    def get_pBreed(self):
+        """
+        getter function for probability to breed.
+        """
+        return self._pBreed
 
 class Predator(Agent):
     """
     This class is derived from the Agent class.
     """
 
-    __slots__ = ['_FoodReserve', '_MaxFoodReserve', '_Generation', '_ID']
+    __slots__ = ['_FoodReserve', '_MaxFoodReserve', '_Generation', '_ID', '_pBreed']
 
     def __init__(self, FoodReserve, Generation=0, MaxFoodReserve=None, pBreed=0.2):
         super().__init__(FoodReserve, Generation, MaxFoodReserve)
         self._ID = "J" + self._ID  # J for jäger. this kinda makes the whole kin thing obsolete.
+        self._pBreed = pBreed
 
+    def get_pBreed(self):
+        """
+        getter function for probability to breed.
+        """
+        return self._pBreed
 
 class Grid:
     """
-    This class provides a different approach to the model, since it doesn't rely on dictionaries nor AgentIDs nor positions
+    This class provides a different approach to the model, since it doesn't rely on dictionaries,
+    nor AgentIDs nor positions.
+    It provides the following attributes:
+        - width
+        - height
+        - grid - a numpy array of either empty cells ("") or cells with agents ("J.." or "B..")
+        - preddict - a dictionary of all predators, with IDs as keys and classobjects as values
+        - preydict - same, just with preys
+
+    It also provides the following methods:
+        - populate - populate the empty initial grid with Agents, depending on their inital density
+        - getter functions for amonut of prey/pred, maximum population size = grid size,
+        - get_Nbh - for a given array index, return the indices and contents of the 9-neighbourhood
+        - Die - if agent dies, delete it from the dictionary and empty its array space.
+        - Move - move an agent in a random direction, if possible. if direction is given, it is
+          directly placed there.
+        - Eat - if prey, just increase the FoodReserve. If pred, check the Nbh for possible preys,
+          and try to eat one of them with probability 1-pFlee. If the pred gets to eat, increase
+          its FoodReserve.
+        - createOffspring - with pBreed, create an offspring of the own kind, reduce FoodReserve by
+          3 (which might be optional later on), and place it on a empty cell in its Nbh.
+        - fr_update - wrapped up some loc for easier handling of the FoodReserve update process
+        - TakeAction - basically, the actual simulation step. This function determines the given
+          index' kintype, and acts accordingly to that with the above methods Move, Eat, Die,
+          createOffspring, fr_update...
+        - plot - plots the whole goddamn thing. if you have a list of the numbers of prey & pred at
+          each timestep (-> get_num_pred, ..), you can use them as an input list to create density
+          plots over time.
     """
 
-    __slots__ = ['_width', '_height', '_grid', '_preddict', '_preydict']
+    __slots__ = ['_width', '_height', '_maxPop', '_grid', '_preddict', '_preydict']
 
-    def __init__(self, width, height, rhoprey, rhopred, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred):
+    def __init__(self, width, height, rhoprey, rhopred, foodresPrey, foodresPred,
+                 MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred, pFlee):
         self._width = width
         self._height = height
+        self._maxPop = self._width * self._height  # maximal population
         dt = 'U' + str(len(str(uuid.uuid4())) + 1)  # datatype for array
+        # initialization of empty grid and dictionaries
         self._grid = np.empty(self._width*self._height, dtype=dt)
         self._preddict = dict()
         self._preydict = dict()
 
+        # populate the empty grid with given parameters
+        self._populate(rhoprey, rhopred, foodresPrey, foodresPred, MaxFoodReservePrey,
+                       MaxFoodReservePred, pBreedPrey, pBreedPred, pFlee)
 
-        self._populate(rhoprey, rhopred, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred)
-        self._grid = self._grid.reshape(self._height, self._width)  # reshape the grid after population
+    def _populate(self, rhoprey, rhopred, foodresPrey, foodresPred, MaxFoodReservePrey,
+                  MaxFoodReservePred, pBreedPrey, pBreedPred, pFlee):
+        """
+        Popuate the empty grid!
+        """
+        Nprey = int(rhoprey * self._maxPop)  # number of prey
+        Npred = int(rhopred * self._maxPop)  # number of pred
 
-    def _populate(self, rhoprey, rhopred, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred):
-        Npopmax = self._width * self._height  # maximum population
-        Nprey = int(rhoprey * Npopmax)  # number of prey
-        Npred = int(rhopred * Npopmax)  # number of pred
-
-        idx = np.arange(Npopmax)  # create array of indices
+        idx = np.arange(self._maxPop)  # create array of indices
         np.random.shuffle(idx)  # shuffle indices
 
         for _ in idx[:Nprey]:
-            p = Prey(FoodReserve=foodresPrey, MaxFoodReserve=MaxFoodReservePrey, pBreed=pBreedPrey)
+            p = Prey(FoodReserve=foodresPrey, MaxFoodReserve=MaxFoodReservePrey, pBreed=pBreedPrey,
+                     pFlee=pFlee)
             self._grid[_] = p.get_ID()
             self._preydict[p.get_ID()] = p
 
@@ -113,17 +172,39 @@ class Grid:
             self._grid[_] = p.get_ID()
             self._preddict[p.get_ID()] = p
 
+        # after populating:
+        self._grid = self._grid.reshape(self._height, self._width)  # reshape to height x width
+
+
+    def get_grid(self):
+        """
+        getter function for the grid array.
+        """
+        return self._grid
+
     def get_num_prey(self):
+        """
+        getter function for the number of preys.
+        """
         return len(self._preydict)
 
     def get_num_pred(self):
+        """
+        getter function for number of preds.
+        """
         return len(self._preddict)
 
     def get_max_pop(self):
-        return self._width * self._height
+        """
+        getter function for the maximal number of possible agents on the grid = grid size
+        """
+        return self._maxPop
 
     def get_Nbh(self, index):
-        # 9 neighbourhood
+        """
+        Return the array indices of self._grid and their content of the 9-neighbourhood for a given
+        index.
+        """
         y,x = index
         delta = [-1,0,1]
         nbh = []
@@ -150,6 +231,12 @@ class Grid:
         self._grid[y,x] = ""  # clear the place in the array
 
     def Move(self, index, direction=None):
+        """
+        check the 9-neighbourhood and pick a random empty place to move to. if none is available,
+        do nothing.
+        if a direction is given (in form of a tuple or a list of length 2), move the agent directly
+        there.
+        """
         y, x = index
         if(direction):
             j, i = direction
@@ -168,36 +255,40 @@ class Grid:
                     self._grid[j,i] = self._grid[y,x]
                     self._grid[y,x] = ""
 
-    def Eat(self, index, agent, pFlee):
+    def Eat(self, index, agent):
+        """
+        find all preys in the Nbh, and pick one at random to eat (or at least try to eat).
+        """
         y, x = index
         idx_nbh, nbh = self.get_Nbh(index)
         preys = []
+        empties = []
         for n in nbh:
             if(len(n)):
                 if n[0] == "B":
                     preys.append(n)
+            else:
+                empties.append(n)
 
         if(len(preys)):
             roll = np.random.rand()
-            if(roll > pFlee):
-                food = preys[np.random.choice(len(preys))]
+            food = preys[np.random.choice(len(preys))]
+            if(roll > self._preydict[food].get_pFlee()):
                 foodidx = idx_nbh[nbh.index(food)]
                 self.fr_update(agent)
                 self.Die(foodidx)
                 self.Move(index, foodidx)
+
+        # if there are no preys to eat, take a step in a random free direction
+        elif(len(empties)):
+            self.Move(index, direction=empties[np.random.choice(len(empties) )])
+
         else:
-            self.Move(index)
+            pass  # TODO, something to do, if you can't move? hm...
 
-
-    def createOffspring(self, agent, kin, index, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred,  pBreedPrey, pBreedPred):
+    def createOffspring(self, agent, kin, index, foodresPrey, foodresPred):
         y, x = index
         idx_nbh, nbh = self.get_Nbh(index)
-        #ID = self._grid[y,x]
-        #if(ID[0] == "B"):
-        #    agent = self._preydict[ID]
-        #else:
-        #    agent = self._preddict[ID]
-
         possibleMoves = []
         for i, n in zip(idx_nbh, nbh):
             if(n == ""):
@@ -205,17 +296,18 @@ class Grid:
 
         if(len(possibleMoves) > 0):
             k, j = possibleMoves[np.random.choice(len(possibleMoves))]
-            agent.set_fr(agent.get_fr() - 3) # reduce foodreserve
+            agent.set_fr(agent.get_fr() - 3) # reduce foodreserve, TODO, maybe 4?
             if(kin == "B"):
-                p = Prey(FoodReserve=foodresPrey, MaxFoodReserve=MaxFoodReservePrey, pBreed=pBreedPrey)
+                p = Prey(FoodReserve=foodresPrey, MaxFoodReserve=agent.get_maxfr(),
+                         pBreed=agent.get_pBreed())
                 self._grid[k,j] = p.get_ID()
                 self._preydict[p.get_ID()] = p
-                #print('*' + p.get_ID())
+
             else:
-                p = Predator(FoodReserve=foodresPred, MaxFoodReserve=MaxFoodReservePred, pBreed=pBreedPred)
+                p = Predator(FoodReserve=foodresPred, MaxFoodReserve=agent.get_maxfr(),
+                             pBreed=agent.get_pBreed())
                 self._grid[k,j] = p.get_ID()
                 self._preddict[p.get_ID()] = p
-                #print('*' + p.get_ID())
 
         else:
             pass
@@ -227,24 +319,22 @@ class Grid:
             agent.set_fr(agent.get_maxfr())
 
 
-    def TakeAction(self, index, pFlee, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred):
+    def TakeAction(self, index, foodresPrey, foodresPred):
         #TODO  pBreed, pFlee as attributes in agent class?
         y, x = index  # for given index, get array indices
 
         if(len(self._grid[y,x])):  # if picked index is not empty
             ID = self._grid[y,x]  # extract ID
             kin = ID[0]  # extract kintype
+
             # get agent object
             if(kin == "B"):
                 agent = self._preydict[ID]
-                pBreed = pBreedPrey
 
             else:
                 agent = self._preddict[ID]
-                pBreed = pBreedPred
 
-            fr = agent.get_fr()
-            # if food reserve is too low, the agent dies
+            fr = agent.get_fr()  # if food reserve is too low, the agent dies
             if(fr -1 <= 0):
                 self.Die(index)
 
@@ -254,30 +344,39 @@ class Grid:
                     self.fr_update(agent)  # food reserve update
 
                 else:
-                    self.Eat(index, agent, pFlee)
+                    self.Eat(index, agent)
 
-
-                if(agent.get_fr() > agent.get_maxfr()//2): # if foodreserve is more than half the maximum
+                if(agent.get_fr() > agent.get_maxfr()//2): # if foodreserve is > than half the maximum
                     roll = np.random.rand()  # pick a random number
-                    if(roll<=pBreed):  # if pick succesfull, breed.
-                        self.createOffspring(agent, kin, index, foodresPrey, foodresPred, MaxFoodReservePrey, MaxFoodReservePred, pBreedPrey, pBreedPred) # Breeding
+                    if(roll<=agent.get_pBreed()):  # if pick succesfull, breed.
+                        self.createOffspring(agent, kin, index, foodresPrey, foodresPred) # Breeding
 
                 if(kin == "B"):
                     self.Move(index)  # otherwise, take a step in a random direction, if possible
         else:
-            pass
+            pass  # something to do for empty grid cells?
 
-    def plot(self, densities=None, currenttimestep=None, timesteps=1000, title='', figsize=(9,12), colourbar=True, ticks=False, filepath='plots/', filename='', dpi=300, fmt='png'):
+    def plot(self, densities=None, currenttimestep=None, timesteps=1000, title='', figsize=(9,12),
+             colourbar=True, ticks=False, filepath='plots/', filename='', dpi=300, fmt='png'):
         # the code below assumes, that self._grid is a numpy array of strings.
         plotarr = np.zeros(shape=(self._height, self._width))
 
-        for j in range(self._height):
-            for i in range(self._width):
-                if(len(self._grid[j,i])):
-                    if(self._grid[j,i][0] == "B"):
-                        plotarr[j,i] = 1
-                    else:
-                        plotarr[j,i] = -1
+        # numpy magic!
+        _y, _x = np.where(self._grid != '')
+
+        for j, i in np.array([_y, _x]).T:
+            if(self._grid[j,i][0] == 'B'):
+                plotarr[j,i] = 1
+            else:
+                plotarr[j,i] = -1
+
+        #for j in range(self._height):
+        #    for i in range(self._width):
+        #        if(len(self._grid[j,i])):
+        #            if(self._grid[j,i][0] == "B"):
+        #                plotarr[j,i] = 1
+        #            else:
+        #                plotarr[j,i] = -1
 
         if(densities):
             prey, pred = densities
@@ -285,13 +384,15 @@ class Grid:
             Rhoprey = np.array(prey)/maxpop
             Rhopred = np.array(pred)/maxpop
             x = np.arange(currenttimestep+2)  # +1 because there is the initial datapoint and because range starts at 0
-            fig, (ax, axd) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3,1]}) # figure setup
+
+            # figure setup
+            fig, (ax, axd) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3,1]})
             w, h = figsize
             fig.set_figheight(h)
             fig.set_figwidth(w)
-            #fig.tight_layout()
             fig.subplots_adjust(hspace=0.1)
 
+            # density plots setup
             axd.plot(x, Rhoprey, color='#fde725', ls='-', label='Prey')
             axd.plot(x, Rhopred, color='#440154', ls='-', label='Predator')
             axd.set_ylabel('Agent density')
