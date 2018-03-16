c120e66946427a5155a67c1c373fdd25c0b021b0
Fabian Krautgasser <fkrautgasser@posteo.org>
2018-03-16 17:42:43 +0100

    added timestamp function to tools

--- actor-critic/tools.py
+++ actor-critic/tools.py
@@ -1,1 +1,16 @@
 """Docstring for the tools."""
+import datetime as dt
+
+
+def timestamp(return_obj: bool=False):
+    """Create and return a timestamp.
+
+    Args:
+        return_obj (bool): if True, return the datetime object instead of the
+            string. Useful for datetime differences.
+    """
+    if return_obj:
+        return dt.datetime.now()
+
+    else:
+        return str(dt.datetime.now())
