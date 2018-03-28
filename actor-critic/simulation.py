693272c671759b8e677e396eec5bb39f2c3e3e64
Fabian Krautgasser <fkrautga@iup.uni-heidelberg.de>
2018-03-28 13:41:35 +0200

    trying to log data

--- actor-critic/simulation.py
+++ actor-critic/simulation.py
@@ -32,6 +32,11 @@ PredatorModel = ac.Policy()
 PreyOptimizer = optim.Adam(PreyModel.parameters(), lr=3e-2)  # whatever the numbers...
 PredatorOptimizer = optim.Adam(PredatorModel.parameters(), lr=3e-2)
 
+# means list
+mean_gens = []  # in step units
+mean_prey_rewards = []  # in episode units
+mean_pred_rewards = []
+
 
 # main loop
 def main():
@@ -39,11 +44,11 @@ def main():
     for i_eps in range(cfg['Sim']['episodes']):  # for now
         print("\n: Environment resetting now...")
         state, idx = env.reset()  # returns state and object of random agent
-        print("::: Plotting current state...")
-        env.render(episode=i_eps, step=-1, **cfg['Plot'])
 
         for _ in range(cfg['Sim']['steps']):
             print(":: Episode {}, Step {}".format(i_eps, _))
+            print("::: Plotting current state...")
+            env.render(episode=i_eps, step=_, **cfg['Plot'])
             while(env.shuffled_agent_list):
                 # if any prey got eaten last round, use it
                 if len(env.eaten_prey) != 0:
@@ -80,8 +85,6 @@ def main():
 
             if done:
                 break
-            print("::: Plotting current state...")
-            env.render(episode=i_eps, step=_, **cfg['Plot'])
             env.create_shuffled_agent_list()
             print("::: Created new shuffled agents list with {} individuals."
                   "".format(len(env.shuffled_agent_list)))
@@ -89,16 +92,28 @@ def main():
             gens = []
             for a in env._agents_dict.values():
                 gens.append(a.generation)
+
+            prey_foodres = []
+            for a in env._agents_tuple.Prey.values():
+                prey_foodres.append(a.food_reserve)
+            print("::: Mean food_reserve of preys: {}"
+                  "".format(np.mean(prey_foodres)))
             print("::: Mean generation: {}".format(np.mean(gens)))
+            mean_gens.append(np.mean(gens))
             idx = env.shuffled_agent_list.pop()
             env.state = env.index_to_state(index=idx)
             state = env.state
 
+        mean_prey_rewards.append(np.mean(PreyModel.rewards))
+        mean_pred_rewards.append(np.mean(PredatorModel.rewards))
         print(": optimizing now...")
         ac.finish_episode(model=PreyModel, optimizer=PreyOptimizer, gamma=0.01,
                           prnt="Preys")
         ac.finish_episode(model=PredatorModel, optimizer=PredatorOptimizer,
                           gamma=0.01, prnt="Predators")
+
+    for f in [mean_gens, mean_pred_rewards, mean_prey_rewards]:
+        np.savetxt(cfg['Plot']['filepath'] + str(f) + ".txt", np.arry(f))
 
 
 if __name__ == "__main__":
