fixed a huge bug the calculation of the loss.
still doesn't work, but its better than before.

actor-critic/actor_critic.py
@@ -29,8 +29,8 @@ class Policy(nn.Module):
     """
 
     # slots -------------------------------------------------------------------
-    __slots__ = ['saved_actions', 'rewards', 'affine1', 'action_head',
-                 'value_head']
+    __slots__ = ['saved_actions', 'rewards', 'affine1', 'affine2', 'affine3',
+                 'action_head', 'value_head']
 
     # init --------------------------------------------------------------------
     def __init__(self, inputs=10, outputs=27, *args, **kwargs):
@@ -41,18 +41,22 @@ class Policy(nn.Module):
         super(Policy, self).__init__()  # call nn.Modules init
 
         # initialize attributes
-        self.saved_actions = []
-        self.rewards = []
+        # self.saved_actions = []
+        # self.rewards = []
 
         # initialize layers
-        self.affine1 = nn.Linear(inputs, 128)  # first layer
-        self.action_head = nn.Linear(128, outputs)  # 27 possible actions
-        self.value_head = nn.Linear(128, 1)
+        self.affine1 = nn.Linear(inputs, 64)  # first layer
+        self.affine2 = nn.Linear(64, 128)  # second layer
+        self.affine3 = nn.Linear(128, 256)  # third layer
+        self.action_head = nn.Linear(256, outputs)  # 27 possible actions
+        self.value_head = nn.Linear(256, 1)
 
     # methods -----------------------------------------------------------------
     def forward(self, input_vector: torch.Tensor) -> tuple:
         """Forward the given input_vector through the layers and return the two outputs."""
         input_vector = F.relu(self.affine1(input_vector))  # Layer 1
+        input_vector = F.relu(self.affine2(input_vector))  # Layer 2
+        input_vector = F.relu(self.affine3(input_vector))  # Layer 3
         action_scores = self.action_head(input_vector)  # action layer
         state_value = self.value_head(input_vector)  # state value layer
 
@@ -61,60 +65,67 @@ class Policy(nn.Module):
 
 
 # defining necessary functions - move to a class maybe? /shrug
-def select_action(*, model, state):
+def select_action(*, model, agent, state):
     """Select an action based on the weighted possibilities given as the output from the model."""
+    agent.memory.States.append(state)  # save the state
     state = torch.from_numpy(state).float()  # float creates a float tensor
     probs, state_value = model(Variable(state))  # propagate the state as Variable
     cat_dist = Categorical(probs)  # categorical distribution
     action = cat_dist.sample()  # I think I should e-greedy right at this point
-    model.saved_actions.append(SavedAction(cat_dist.log_prob(action),
-                                           state_value))
+    agent.memory.Actions.append(SavedAction(cat_dist.log_prob(action),
+                                            state_value))
     return action.data[0]  # just output a number and not additionally the type
 
 
 # defining what to do after the episode finished.
-def finish_episode(*, model, optimizer, gamma: float=0.1, prnt: str=None):
+def finish_episode(*, model, optimizer, history, gamma: float=0.1) -> None:
     """Calculate the losses and backprop them through the models NN."""
     # initialize a few variables
-    R = 0  # The discounted reward
-    policy_losses = []
-    state_value_losses = []
-    rewards = []
     eps = np.finfo(np.float32).eps  # machine epsilon
+    losses = []
+    for (_, agent_rewards, saved_actions) in history:
 
-    # get saved actions
-    saved_actions = model.saved_actions
+        R = 0  # The discounted reward
+        rewards = []
+        policy_losses = []
+        state_value_losses = []
 
-    # iterate over all rewards that we got during the play
-    for r in model.rewards[::-1]:  # backwards to account for the more recent actions
-        R = r + gamma * R  # discount!
-        rewards.append(R)  # append + [::-1] is faster than insert(0,*)
+        # get saved actions
+        # saved_actions = model.saved_actions
 
-    rewards = torch.Tensor(rewards[::-1])  # backwardss
-    rewards = (rewards - rewards.mean()) / (rewards.std() + eps)  # why eps???
+        # iterate over all rewards that we got during the play
+        for r in agent_rewards[::-1]:  # backwards to account for the more recent actions
+            R = r + gamma * R  # discount!
+            rewards.append(R)  # append + [::-1] is faster than insert(0,*)
 
-    # now interate over all probability-state value-reward pairs
-    for (log_prob, state_value), r in zip(saved_actions, rewards):
-        reward = r - state_value.data[0]  # get the value, needs `Variable`
-        policy_losses.append(-log_prob * reward)
-        # calculate the (smooth) L^1 loss = least absolute deviation
-        state_value_losses.append(F.smooth_l1_loss(state_value,
-                                                   Variable(torch.Tensor([r]))))
+        rewards = torch.Tensor(rewards[::-1])  # backwardss
+        rewards = (rewards - rewards.mean()) / (rewards.std() + eps)  # why eps???
 
-    # empty the gradient of the optimizer
-    optimizer.zero_grad()
+        # now interate over all probability-state value-reward pairs
+        for (log_prob, state_value), r in zip(saved_actions, rewards):
+            reward = r - state_value.data[0]  # get the value, needs `Variable`
+            policy_losses.append(-log_prob * reward)
+            # calculate the (smooth) L^1 loss = least absolute deviation
+            state_value_losses.append(F.smooth_l1_loss(state_value,
+                                      Variable(torch.Tensor([r]))))
 
-    # calculate the loss
-    loss = torch.stack(policy_losses).sum() + torch.stack(state_value_losses).sum()
+        # empty the gradient of the optimizer
+        optimizer.zero_grad()
+
+        # calculate the loss
+        losses.append(torch.stack(policy_losses).sum() +
+                      torch.stack(state_value_losses).sum())
+
+    # average all losses
+    loss = torch.stack(losses).mean()
+    print(loss)
 
     # backpropagate the loss
     loss.backward()
     optimizer.step()
 
-    if prnt is not None:
-        # print mean rewards
-        print(": mean rewards for {}: {}".format(prnt, np.mean(model.rewards)))
+    # TODO return some mean values or so
 
     # clear memory from unneeded variables
-    del model.rewards[:]
-    del model.saved_actions[:]
+    #del model.rewards[:]
+    #del model.saved_actions[:]

actor-critic/environment.py
@@ -5,12 +5,14 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 from collections import namedtuple
-from typing import Union, Callable
+from typing import Union, Callable, NamedTuple
 from gym.spaces import Discrete, Tuple  # for the discrete action space of the agents
 from gym.utils import seeding
 
 from tools import DeepChainMap, type_check, timestamp
 import actor_critic as ac
+
+hist = namedtuple('history', ('Predator', 'Prey'))
 
 
 class Environment:
@@ -22,11 +24,11 @@ class Environment:
     # slots -------------------------------------------------------------------
     __slots__ = ['_dim', '_densities', '_agent_types', '_agent_kwargs',
                  '_max_pop', '_env', '_agents_dict', '_agents_tuple',
-                 '_np_random']  # _agent_named_properties
+                 '_np_random', '_history']  # _agent_named_properties
 
     # init --------------------------------------------------------------------
     def __init__(self, *, dim: tuple, agent_types: Union[Callable, tuple],
-                 densities: Union[float, tuple],
+                 densities: Union[float, tuple], history: tuple=None,
                  **agent_kwargs: Union[int, float, None]):
         """Initialize the environment.
 
@@ -38,6 +40,7 @@ class Environment:
         self._agent_kwargs = {}
         self._agent_types = None
         self._np_random = None
+        self._history = None  # keeps every memory of every agent
 
         # set property managed attribute(s)
         self.dim = dim
@@ -64,6 +67,12 @@ class Environment:
         # initialise with empty dicts
         self._agents_tuple = agnts(*[{} for _ in self.agent_types])
         self._agents_dict = DeepChainMap(*self._agents_tuple)
+
+        # initialize history
+        if history:
+            self.history = history
+        else:
+            self.history = hist([], [])  # empty history
 
     # properties -------------------------------------------------------------
     # dimensions
@@ -157,6 +166,23 @@ class Environment:
     def max_pop(self) -> int:
         """Return the maximum population of the grid."""
         return self._max_pop
+
+    @property
+    def history(self) -> NamedTuple:
+        """Return the list of recorded deeds."""
+        return self._history
+
+    @history.setter
+    def history(self, history: tuple) -> None:
+        """Define the past by setting history."""
+        if not isinstance(history, tuple):
+            raise TypeError("history must be of type list but {} was given."
+                            "".format(type(history)))
+        elif self.history is not None:
+            raise RuntimeError("history has already started, there is no "
+                               "forgiveness anymore.")
+        else:
+            self._history = history
 
     # staticmethods -----------------------------------------------------------
 
@@ -367,7 +393,11 @@ class GridPPM(Environment):
         """Delete the given index from the environment and replace its position with empty string ''."""
         uuid = self.env[tuple(index)]
         if uuid != '':
+            ag = self._agents_dict[uuid]
+            # record history in the right list
+            getattr(self.history, ag.kin).append(ag.memory)
             del self._agents_dict[uuid]  # only deletes the dict entry
+            del ag
             self.env[tuple(index)] = ''
         else:
             warnings.warn("Trying to delete an empty cell", RuntimeWarning)
@@ -388,6 +418,12 @@ class GridPPM(Environment):
         """
         self._add_to_agents_tuple(newborn=newborn)
         self.env[tuple(target_index)] = newborn.uuid  # we assume that the index is not occupied
+
+    # index to agent
+    @_index_test_ndarray
+    def _idx_to_ag(self, index: np.ndarray) -> Callable:
+        """Return the agent object corresponding to a given index."""
+        return self._agents_dict[self.env[tuple(index)]]
 
     # create shuffled list of agents
     def create_shuffled_agent_list(self) -> list:
@@ -420,18 +456,32 @@ class GridPPM(Environment):
 
         If agent was prey and got eaten, index points to '' in env. The return for food_reserve is then None.
         """
-        neighbourhood, _ = self.neighbourhood(index=index)
-        state = [self._uuid_to_int(uuid=uuid) for uuid in neighbourhood]
-
-        # check if agent actually exists FIXME: hardcoding
+        # it can happen, that the index points to an empty space in the environment. This is due to the fact, that an index in the shuffled list doesn't get removed, if a prey got eaten. thus, empty cells are just ignored.
+        state = []
+        # check if agent has memory:
         if ag is not None:
-            state.append(ag.food_reserve)  # got handed an agent
-        elif neighbourhood[4] == "":  # agent died, i.e. got eaten or starved
-            state.append(None)  # there is no food_reserve FIXME causes problems
-        else:
-            state.append(self._agents_dict[neighbourhood[4]].food_reserve)
+            if ag.memory.States:
+                state = ag.memory.States[-1]
+                return state
 
-        return np.array(state)
+            else:
+                neighbourhood, _ = self.neighbourhood(index=index)
+                state = [self._uuid_to_int(uuid=uuid) for uuid in neighbourhood]
+                state.append(ag.food_reserve)  # got handed an agent
+                return np.array(state)
+
+        else:
+            active_agent = self._idx_to_ag(index)
+            if active_agent.memory.States:
+                state = active_agent.memory.States[-1]  # remember the latest state
+                return state
+
+            else:
+                neighbourhood, _ = self.neighbourhood(index=index)
+                state = [self._uuid_to_int(uuid=uuid) for uuid in neighbourhood]
+                state.append(active_agent.food_reserve)
+
+                return np.array(state)
 
     # neighbourhood
     @_index_test_ndarray
@@ -496,8 +546,7 @@ class GridPPM(Environment):
 
             # check if eating move is possible:
             # fetch agent
-            agent_uuid = self.env[tuple(index)]
-            agent = self._agents_dict[agent_uuid]
+            agent = self._idx_to_ag(index)
 
             # initialize target
             target_agent = None
@@ -505,7 +554,7 @@ class GridPPM(Environment):
             if type(agent) not in self.agent_types:
                 raise RuntimeError("The current agent {} of kintype {} is not "
                                    "in the list agent_types. This should not "
-                                   "have happened!".format(agent_uuid,
+                                   "have happened!".format(agent.uuid,
                                                            agent.kin))
 
             # now we have to check if target is eatable or if there is
@@ -583,13 +632,13 @@ class GridPPM(Environment):
                                 "".format(np.ndarray, type(index)))
 
             # fetch agent
-            agent_uuid = self.env[tuple(index)]
-            agent = self._agents_dict[agent_uuid]
+            agent = self._idx_to_ag(index)
 
             if type(agent) not in self.agent_types:
+
                 raise RuntimeError("The current agent {} of kintype {} is not "
                                    "in the list agent_types. This should not "
-                                   "have happened!".format(agent_uuid,
+                                   "have happened!".format(agent.uuid,
                                                            agent.kin))
 
             # now we have to check if target space is free or if it is occupied
@@ -602,7 +651,7 @@ class GridPPM(Environment):
                     # can't procreate without space
                     return self.REWARDS['wrong_action']
 
-                elif target_uuid == agent_uuid:
+                elif target_uuid == agent.uuid:
                     # don't try to create offspring in your own cell
                     return self.REWARDS['wrong_action']
 
@@ -652,6 +701,10 @@ class GridPPM(Environment):
         # empty eaten prey list
         self.eaten_prey = []
 
+        # clear history
+        del self.history.Predator[:]
+        del self.history.Prey[:]
+
         # pop list and return state
         index = self.shuffled_agent_list.pop()
         self.state = self.index_to_state(index=index)
@@ -680,6 +733,9 @@ class GridPPM(Environment):
                 raise RuntimeError("reward should not be of type None! The"
                                    " last action was {} by agent {}"
                                    "".format(act, agent))
+
+        # save the reward
+        agent.memory.Rewards.append(reward)
 
         if (len(self._agents_tuple.Predator) and len(self._agents_tuple.Prey)) is 0:
             done = True  # at least one species died out

actor-critic/simulation.py
@@ -24,13 +24,13 @@ with open("simulation_config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile)
 
 env = GridPPM(agent_types=(Predator, Prey), **cfg['Model'])
-env.seed(12345678)
+#env.seed(12345678)
 
 # Initialize the policies and optimizer ---------------------------------------
 PreyModel = ac.Policy()
 PredatorModel = ac.Policy()
-PreyOptimizer = optim.Adam(PreyModel.parameters(), lr=3e-2)  # whatever the numbers...
-PredatorOptimizer = optim.Adam(PredatorModel.parameters(), lr=3e-2)
+PreyOptimizer = optim.Adam(PreyModel.parameters(), lr=1e-4)  # whatever the numbers...
+PredatorOptimizer = optim.Adam(PredatorModel.parameters(), lr=1e-4)
 
 # means list
 mean_gens = []  # in step units
@@ -58,25 +58,27 @@ def main():
                         env.state[-1] = int(ag.food_reserve)
                     state = env.state
                     model = PreyModel
-                    action = ac.select_action(model=model, state=state)
+                    action = ac.select_action(model=model, agent=ag,
+                                              state=state)
                     reward, state, done, idx = env.step(model=model,
                                                         agent=ag,
                                                         index=tmpidx,
                                                         returnidx=idx,
                                                         action=action)
                 else:
-                    ag = env._agents_dict[env.env[tuple(idx)]]  # agent object
+                    ag = env._idx_to_ag(idx)  # agent object
 
                     # select model and action
                     model = PreyModel if ag.kin == "Prey" else PredatorModel
-                    action = ac.select_action(model=model, state=state)
+                    action = ac.select_action(model=model, agent=ag,
+                                              state=state)
                     # take a step
                     reward, state, done, idx = env.step(model=model,
                                                         agent=ag,
                                                         index=idx,
                                                         action=action)
 
-                model.rewards.append(reward)
+                # model.rewards.append(reward)
                 if done:
                     print(":: Breakpoint reached: Predators: {}\t Prey: {}"
                           "".format(len(env._agents_tuple.Predator),
@@ -93,27 +95,23 @@ def main():
             for a in env._agents_dict.values():
                 gens.append(a.generation)
 
-            prey_foodres = []
-            for a in env._agents_tuple.Prey.values():
-                prey_foodres.append(a.food_reserve)
-            print("::: Mean food_reserve of preys: {}"
-                  "".format(np.mean(prey_foodres)))
             print("::: Mean generation: {}".format(np.mean(gens)))
             mean_gens.append(np.mean(gens))
+
             idx = env.shuffled_agent_list.pop()
             env.state = env.index_to_state(index=idx)
             state = env.state
 
-        mean_prey_rewards.append(np.mean(PreyModel.rewards))
-        mean_pred_rewards.append(np.mean(PredatorModel.rewards))
+        # mean_prey_rewards.append(np.mean(PreyModel.rewards))
+        # mean_pred_rewards.append(np.mean(PredatorModel.rewards))
         print(": optimizing now...")
-        ac.finish_episode(model=PreyModel, optimizer=PreyOptimizer, gamma=0.01,
-                          prnt="Preys")
+        ac.finish_episode(model=PreyModel, optimizer=PreyOptimizer,
+                          history=env.history.Prey)
         ac.finish_episode(model=PredatorModel, optimizer=PredatorOptimizer,
-                          gamma=0.01, prnt="Predators")
+                          history=env.history.Predator)
 
-    for f in [mean_gens, mean_pred_rewards, mean_prey_rewards]:
-        np.savetxt(cfg['Plot']['filepath'] + str(f) + ".txt", np.array(f))
+    # for f in [mean_gens, mean_pred_rewards, mean_prey_rewards]:
+    #    np.savetxt(cfg['Plot']['filepath'] + str(f) + ".txt", np.array(f))
 
 
 if __name__ == "__main__":